e585b0f876939219a56786b922de4ab9e312733d
LimitlessSocks <socktheringleader@gmail.com>
2023-08-04 21:39:03 -0400

    add physical log files

--- src/timed-console.js
+++ src/timed-console.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const TimedConsole = {
     formatter: new Intl.DateTimeFormat("en-US", {
         year: "numeric",
@@ -10,15 +12,65 @@ const TimedConsole = {
         fractionalSecondDigits: 3,
         format: "MM/dd/yyyy HH:mm:ss.SSS",
     }),
+    uuid: null,
+    stderrHandle: null,
+    stdoutHandle: null,
+    writeQueue: Promise.resolve(),
+    
     getPrefix() {
         return `[Database ${this.formatter.format(new Date())}]`;
     },
     log(...args) {
-        console.log(this.getPrefix(), ...args);
+        let prefix = this.getPrefix();
+        console.log(prefix, ...args);
+        this.appendToFile(this.stdoutHandle,
+            `${prefix} ${args.map(arg => String(arg)).join(" ")}`
+        );
     },
     error(...args) {
-        console.error(this.getPrefix(), ...args);
+        let prefix = this.getPrefix();
+        console.error(prefix, ...args);
+        this.appendToFile(this.stderrHandle,
+            `${prefix} ${args.map(arg => String(arg)).join(" ")}`
+        );
     },
+    
+    async appendToFile(filename, data) {
+        this.writeQueue = this.writeQueue
+            .then(() =>
+                new Promise((resolve, reject) => {
+                    fs.appendFile(filename, `${data}\n`, err => {
+                        if(err) {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
+                })
+            )
+            .catch(err => {
+                // for obvious reasons, this cannot use the timed console call
+                console.error(`[${this.getPrefix()}] Unlogged error appending to ${filename}: ${err}`);
+            });
+
+        await this.writeQueue;
+    },
+    initialize() {
+        const LOGS_FOLDER = "./logs";
+        
+        if(!fs.existsSync(LOGS_FOLDER)) {
+            fs.mkdirSync(LOGS_FOLDER);
+        }
+        
+        this.uuid = this.formatter.format(new Date()).replace(/[^0-9]+/g, "-");
+        this.stdoutHandle = `${LOGS_FOLDER}/${this.uuid}.stdout.log`;
+        this.stderrHandle = `${LOGS_FOLDER}/${this.uuid}.stderr.log`;
+
+        this.log("== Start of output log ==");
+        this.error("== Start of error log ==");
+    }
 };
+
+TimedConsole.initialize();
 
 module.exports = TimedConsole;
